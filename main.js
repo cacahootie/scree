9daf7e19c47c72a02724664454f33c2b1dbd9eef
Brad Smith <brad.alan.smith@gmail.com>
2016-10-20 10:55:39 -0700

    basic functionality

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const appFactory = require('./lib/app_factory')
+
+var main = function(){
+    var app_instance = appFactory.get_running()
+}
+
+main()
---
+++ test/test_local_mode.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const assert = require('chai').assert,
+      request = require('supertest')
+
+const app_factory = require('../lib/app_factory')
+
+
+describe('json upload and validation', function(){
+    let app
+
+    beforeEach(function() {
+        app = app_factory.get_instance('.', 'testing')
+    })
+
+    it('registers as testing mode', function(done) {
+        if (process.env.NODE_ENV) {
+            done()
+        } else {
+            assert(app.env.NODE_ENV === 'testing')
+            done()
+        }
+    })
+
+    it('validates a simple example', function(done) {
+        request(app)
+            .post('/city')
+            .send({
+                "city": "Phoenix",
+                "state": "Arizona",
+                "zip": 85004,
+                "county": "Maricopa",
+                "country": "USA"
+            })
+            .expect(d => d === "good")
+            .expect(200, done)
+    })
+
+    it('fails a simple example', function(done) {
+        request(app)
+            .post('/city')
+            .send({
+                "tronald": "dump",
+                "cilary": "hlinton"
+            })
+            .expect(d => d === "crap")
+            .expect(200, done)
+    })
+})
